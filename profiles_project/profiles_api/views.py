c54285ac814d37a9a3fd76e1d34e1812122ed5fc
Shubham Maurya <shbhmmry@gmail.com>
2022-06-24 13:52:52 +0530

    auth token added

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -3,11 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from profiles_api import serializers
+from rest_framework import filters
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 from profiles_api import models
 from profiles_api import permissions
-...
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
@@ -127,3 +130,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnprofile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+   
